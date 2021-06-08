7569a504bf5e8abb1e9c226994ff968eee85438f
kanchaswapna <75799923+kanchaswapna@users.noreply.github.com>
2021-06-08 17:38:53 +0530

    main

--- businessGroup/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from .models import BusinessGroup, Level, Module, Batch
-
-admin.site.register(Batch)
-admin.site.register(BusinessGroup)
-admin.site.register(Level)
-admin.site.register(Module)
